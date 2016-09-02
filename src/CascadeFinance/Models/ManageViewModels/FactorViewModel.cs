ea11723893af528df48038bdce333e49f0f44b64
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2016-09-01 21:08:32 -0400

    Add project files.

---
+++ src/CascadeFinance/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CascadeFinance.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
