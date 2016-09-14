7f0ebf3ac0784d5be2b587aa05b7623ee861c214
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2016-09-14 13:12:40 -0400

    Generate initial ef database migration for new models

--- src/CascadeFinance/Data/Migrations/20160914170731_coreMigration.Designer.cs
+++ src/CascadeFinance/Data/Migrations/20160914170731_coreMigration.Designer.cs
@@ -8,9 +8,10 @@ using CascadeFinance.Data;
 namespace CascadeFinance.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160914170731_coreMigration")]
+    partial class coreMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
