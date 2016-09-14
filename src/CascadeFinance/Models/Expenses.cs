783ac2eeee488f9ec5ab2c4470026107d107e691
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2016-09-14 12:59:41 -0400

    Adds basic models for Widgets and Expenses

---
+++ src/CascadeFinance/Models/Expenses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CascadeFinance.Models
+{
+    public class Expenses
+    {
+        [Key]
+        public int ExpensesId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Value { get; set; }
+
+        [Required]
+        public DateTime ExpenseDate { get; set; }
+
+        [Required]
+        public string Tag { get; set; }
+
+        //Foreign Key
+        public int WidgetId { get; set; }
+
+        public Widgets Widget { get; set; }
+    }
+}
