76754650d15e5448f3feaa459ba394168c7acbfe
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2016-10-23 19:24:18 -0400

    Add basic request to Plaid Client for account and transaction data

--- src/CascadeFinance.Plaid/PlaidClient.cs
+++ src/CascadeFinance.Plaid/PlaidClient.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CascadeFinance.Plaid.request;
+using System;
+using System.Collections.Generic;
 
 public class PlaidClient
 {
@@ -12,4 +14,18 @@ public class PlaidClient
         this.client_id = client_id;
         this.secret = secret;
 	}
+
+    public string requestAllAccountData(Credentials credentials, string institution)
+    {
+        var values = new Dictionary<string, string> {
+            { "client_id", client_id },
+            { "secret", secret},
+            { "username", credentials.username },
+            { "password", credentials.password },
+            { "type", institution},
+        };
+        Request request = new Request();
+        var returnString = request.handleRequest(values);
+        return returnString.Result;
+    }
 }
