79ba79cc8967182d4c6e6afa12f3e2585c84a4d4
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2016-10-23 13:56:21 -0400

    Added some skeleton objects for plaid interaction Mapped Account constructor to recieved JSON for deserialization

---
+++ src/CascadeFinance.Plaid/PlaidClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class PlaidClient
+{
+    private string accessToken;
+
+    private string client_id;
+    private string secret;
+
+	public PlaidClient(string client_id, string secret)
+	{
+        this.client_id = client_id;
+        this.secret = secret;
+	}
+}
