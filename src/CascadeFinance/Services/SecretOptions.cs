1b270513006c8a31362edd581ed7c6a002bfc621
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2016-11-09 17:28:27 -0500

    #16 Added secret support to project Forces application into development mode when running in debug mode Added SecretOptions class

---
+++ src/CascadeFinance/Services/SecretOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace CascadeFinance.Services
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class SecretOptions
+    {
+        public string PlaidSecret { get; set; }
+    }
+}
