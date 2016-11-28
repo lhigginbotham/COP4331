d46121c026b6ab497f834a6e89e4dd8674be804d
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2016-11-28 09:34:53 -0500

    Create basic view model for wizard, add to imports

--- src/CascadeFinance/Controllers/WizardController.cs
+++ src/CascadeFinance/Controllers/WizardController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CascadeFinance.Models.WizardViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CascadeFinance.Controllers
@@ -9,6 +10,11 @@ namespace CascadeFinance.Controllers
     public class WizardController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Submit()
         {
             return View();
         }
