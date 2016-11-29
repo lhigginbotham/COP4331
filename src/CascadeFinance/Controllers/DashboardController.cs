08044c91417834ebcd08a92d2c7c8efa988f1b60
CamiloArturoMontoya <camiloamontoya@knights.ucf.edu>
2016-11-29 17:23:53 -0500

    not finishes but elizabeth needs help page

--- src/CascadeFinance/Controllers/DashboardController.cs
+++ src/CascadeFinance/Controllers/DashboardController.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CascadeFinance.Data;
 
 namespace CascadeFinance.Controllers
 {
     public class DashboardController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
         public IActionResult Dashboard()
         {
             return View();
         }
 
-        public IActionResult Drilldown()
+        public ActionResult Drilldown()
         {
 
+            using (_context)
+            {
+                var table = _context.Expenses;
+
+                var expense = new Models.Expenses { Name ="test", Value = 100.00m, ExpenseDate =new DateTime( 2010, 1, 18), Tag = "food",  WidgetId = 1,  };
+                table.Add(expense);
+                table.SaveChanges();
+
+
+            }
+
             return View();
+            
         }
         public IActionResult VisualAnalytics()
+
+
         {
+
             return View();
         }
     }
