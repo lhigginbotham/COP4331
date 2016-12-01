4abcb4ede24d312a459fb3aebd1ead25661b2092
zbarfield <zbarfield@knights.ucf.edu>
2016-12-01 01:48:10 -0500

    ELIZABETH TOLD ME SO VIEW DATA CONFIRMED KILL STREAK, UAV ONLINE

--- src/CascadeFinance/Controllers/DashboardController.cs
+++ src/CascadeFinance/Controllers/DashboardController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CascadeFinance.Data;
+using Microsoft.AspNetCore.Identity;
+using CascadeFinance.Models;
 
 namespace CascadeFinance.Controllers
 {
     public class DashboardController : Controller
     {
         private ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public DashboardController(ApplicationDbContext context)
+        public DashboardController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
-
+            _userManager = userManager;
         }
         public IActionResult Dashboard()
         {
@@ -41,11 +44,22 @@ namespace CascadeFinance.Controllers
         }
         public IActionResult VisualAnalytics()
         {
+            /* Gather user data */
+            var user = GetCurrentUserAsync();
+
+            /* grab data from db */
+            //var widgetsDb = _context.Widgets
+                                    //.Where(b => b.ApplicationUserId == user.Id)
+                                    //.ToList();
+
             /* Create feaux data */
             Models.Widgets[] widgets = new Models.Widgets[]
             {
                 new Models.Widgets { WidgetId = 0, Name = "Potatoes", Priority = 1, Total = 100, ApplicationUser = new Models.ApplicationUser { Widget = null, BankAccount = null }, ApplicationUserId = 0 },
                 new Models.Widgets { WidgetId = 0, Name = "Tomatoes", Priority = 2, Total = 50, ApplicationUser = new Models.ApplicationUser { Widget = null, BankAccount = null }, ApplicationUserId = 0 },
+                new Models.Widgets { WidgetId = 0, Name = "Hambugers", Priority = 1, Total = 100, ApplicationUser = new Models.ApplicationUser { Widget = null, BankAccount = null }, ApplicationUserId = 0 },
+                new Models.Widgets { WidgetId = 0, Name = "Tacos", Priority = 2, Total = 50, ApplicationUser = new Models.ApplicationUser { Widget = null, BankAccount = null }, ApplicationUserId = 0 },
+                new Models.Widgets { WidgetId = 0, Name = "Goldfish", Priority = 3, Total = 75, ApplicationUser = new Models.ApplicationUser { Widget = null, BankAccount = null }, ApplicationUserId = 0 },
                 new Models.Widgets { WidgetId = 0, Name = "Beans", Priority = 3, Total = 75, ApplicationUser = new Models.ApplicationUser { Widget = null, BankAccount = null }, ApplicationUserId = 0 }
             };
 
@@ -53,6 +67,11 @@ namespace CascadeFinance.Controllers
             ViewData["Data"] = widgets;
 
             return View();
+        }
+
+        private async Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return await _userManager.GetUserAsync(HttpContext.User);
         }
     }
 
