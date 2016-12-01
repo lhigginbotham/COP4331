2d3b89f5bbc9ac06065af695c9cffdb76feda5ad
zbarfield <zbarfield@knights.ucf.edu>
2016-11-30 22:08:01 -0500

    After Login Lands on Dashboard. Rearranged Login Page.

--- src/CascadeFinance/Controllers/AccountController.cs
+++ src/CascadeFinance/Controllers/AccountController.cs
@@ -63,7 +63,7 @@ namespace CascadeFinance.Controllers
                 if (result.Succeeded)
                 {
                     _logger.LogInformation(1, "User logged in.");
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction(nameof(DashboardController.Dashboard), "Dashboard");
                 }
                 if (result.RequiresTwoFactor)
                 {
